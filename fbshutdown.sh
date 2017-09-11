14d47e0738eef7ceaa6935cf4a04d9b08e0cd9db
akattunga <akattunga@gmail.com>
2017-09-11 11:25:27 -0300

    bug

--- fbshutdown.sh
+++ fbshutdown.sh
@@ -9,7 +9,7 @@ for file in $DATAPATH
 do
     DATABASE="localhost:$file"
 
-    echo "$timestamp  database: $file
+    echo "$timestamp  database: $file"
     /opt/firebird/bin/gfix -shut full -force 0 -user $USERNAME -pa $PASSWORD $DATABASE
 
 done
--- fbsweep.sh
+++ fbsweep.sh
@@ -9,7 +9,7 @@ for file in $DATAPATH
 do
     DATABASE="localhost:$file"
 
-    echo "$timestamp  database: $file
+    echo "$timestamp  database: $file"
     /opt/firebird/bin/gfix -sweep -user $USERNAME -pa $PASSWORD $DATABASE
 
 done

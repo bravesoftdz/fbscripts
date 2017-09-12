eba0bdf4d2925744de1de6f1ec3562f3f5a08a75
akattunga <akattunga@gmail.com>
2017-09-12 08:54:12 -0300

    .

--- fbreindex.sh
+++ fbreindex.sh
@@ -1,12 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 #
 
-FLIDXLST=/tmp/idxlst.tmp
+FLIDXLST=/tmp/$(basename $0 .sh)_indexlst.tmp
+CMNDFILE=/tmp/$(basename $0 .sh)_command.tmp
+ERRORCMD=/tmp/$(basename $0 .sh)_errorcmd.tmp
 
 USERNAME=sysdba
 PASSWORD=masterkey
 
-for file in "$@"
+rm -f $ERRORCMD
+
+for file in $@
 do
     DATABASE="localhost:$file"
 
@@ -15,10 +19,18 @@ do
     while read dbindex
     do
         if [ -n "$dbindex" ]; then
+
             timestamp=`date '+%Y-%m-%d %H:%M:%S'`
             echo "$timestamp  database: $file  index: $dbindex"
-            printf "ALTER INDEX \"$dbindex\" active;" | /opt/firebird/bin/isql -u $USERNAME -p $PASSWORD $DATABASE
+            printf "ALTER INDEX \"$dbindex\" active;" > $CMNDFILE
+
+            /opt/firebird/bin/isql -b -i "$CMNDFILE" -u $USERNAME -p $PASSWORD "$DATABASE" > $ERRORCMD 2>&1
+            if [ -s $ERRORCMD ]; then
+                >&2 echo "$timestamp  database: $file  index: $dbindex"
+                >&2 cat $ERRORCMD
+                >&2 echo "--------------------------------------------"
+            fi
         fi
     done < $FLIDXLST
-
 done
+
